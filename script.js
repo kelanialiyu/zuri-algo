710ba0bf848c0c2d8d96dbba3e7281af6370322d
kelani aliyu <kelanialiyu@gmail.com>
2021-05-20 01:01:43 +0100

    added most files

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+function convertFahrToCelsius(input_in_F){
+	var result;
+
+	if(isNaN(input_in_F)) {
+
+		var type = typeof  input_in_F;
+
+		if(Array.isArray(input_in_F)){
+			input_in_F ="["+input_in_F+"]";
+			type = "array";
+
+		}
+		// input_in_F+="";
+		result = `${JSON.stringify(input_in_F)} is not a valid number but ${ get_article(type) } ${type}.`;
+	}
+	else{
+		result = (input_in_F -32) *5/9	
+	}
+	
+	return result;
+}
+
+function is_vowel(word){
+	var first_letter = word.trim().charAt(0).toLowerCase();
+	return (first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u");
+
+}
+
+function get_article(word){
+	var result;
+	if(is_vowel(word)){
+		result = "an";
+	}
+	else{
+		result = "a";
+	}
+	return result;
+}
