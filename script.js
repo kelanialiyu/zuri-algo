a8bec25a383d16a2aeb6aabe99d3b871d217e39a
kelani aliyu <kelanialiyu@gmail.com>
2021-05-20 13:43:21 +0100

    finished the second function and refactored

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// Question 1
 function convertFahrToCelsius(input_in_F){
 	var result;
 
@@ -19,6 +21,61 @@ function convertFahrToCelsius(input_in_F){
 	return result;
 }
 
+// Question 2
+function checkYuGiOh(n){
+	var result
+	if(isNaN(n)){
+		result =`invalid parameter: ${n}`;
+	}
+	else{
+		var result = new Array(n);
+		for (var i = 0; i < n; i++) {
+			result[i] = i+1;
+			if((i+1)%2==0){
+				result[i]="yu"
+			}
+			if((i+1)%3==0){
+				if(isNaN(result[i])){
+					result[i]+="-gi";
+				}
+				else{
+					result[i]="gi"
+				}
+			}
+			if((i+1)%5==0){
+				if(isNaN(result[i])){
+					result[i]+="-oh";
+				}
+				else{
+					result[i]="oh"
+				}
+			}
+		}
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ************helper functions*************************
 function is_vowel(word){
 	var first_letter = word.trim().charAt(0).toLowerCase();
 	return (first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u");
